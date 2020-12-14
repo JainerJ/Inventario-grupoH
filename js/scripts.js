3c25e4f9ca4ba1c3f6ef399059d06668e6e3b73c
Rhaiza <imontesino@uninorte.edu.co>
2020-12-14 12:33:12 -0500

    ismael

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,61 @@
+
+
 function buscar_articulo (){
     var nombre_articulo = document.getElementById("nombre_articulo");
     var comprobador = 0;
     for(x in nombre_articulo.value){
-        if (nombre_articulo.value[x] != " "){ 
+        if (nombre_articulo.value[x] != " "){
             comprobador++;
         }
     }
     if(nombre_articulo.value!="" && comprobador >=1){
+        document.getElementById("demo").innerHTML = "Estas buscando: " + nombre_articulo.value;
         location.href = "contenido.html";
     }
     else{
         window.alert("Debe ingresar un valor");
-    }  
+    }
 }
 
 function regresar_inicio(){
     location.href = "index.html";
 }
 
-function ir_registro(){
-    location.href = "registro.html"
-}
+/* ---------------------------------------------------------JavaScript  3.Resultado-busqueda ----------------------------------------------------------- */
+$(document).ready(function(){
+
+	// AGREGANDO CLASE ACTIVE AL PRIMER ENLACE ====================
+	$('.category_list .category_item[category="all"]').addClass('ct_item-active');
+
+	// FILTRANDO PRODUCTOS  ============================================
+
+	$('.category_item').click(function(){
+		var catProduct = $(this).attr('category');
+		console.log(catProduct);
+
+		// AGREGANDO CLASE ACTIVE AL ENLACE SELECCIONADO
+		$('.category_item').removeClass('ct_item-active');
+		$(this).addClass('ct_item-active');
+
+		// OCULTANDO PRODUCTOS =========================
+		$('.product-item').css('transform', 'scale(0)');
+		function hideProduct(){
+			$('.product-item').hide();
+		} setTimeout(hideProduct,400);
+
+		// MOSTRANDO PRODUCTOS =========================
+		function showProduct(){
+			$('.product-item[category="'+catProduct+'"]').show();
+			$('.product-item[category="'+catProduct+'"]').css('transform', 'scale(1)');
+		} setTimeout(showProduct,400);
+	});
+
+	// MOSTRANDO TODOS LOS PRODUCTOS =======================
+
+	$('.category_item[category="all"]').click(function(){
+		function showAll(){
+			$('.product-item').show();
+			$('.product-item').css('transform', 'scale(1)');
+		} setTimeout(showAll,400);
+	});
+});
